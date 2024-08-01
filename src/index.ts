5879ccceafac66cd1aed2277a374ddf4630af8bf
Rizwan <rizwandev99@gmail.com>
2024-08-01 11:58:16 +0530

    completed

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,20 @@ const client = new Client({
   connectionString: "postgresql://postgres:mysecretpassword@localhost/postgres",
 });
 
-async function createUsersTable() {
+async function createUsersTable(
+  username: string,
+  password: string,
+  email: string
+) {
   await client.connect();
-  const result = await client.query(`
-        CREATE TABLE users2 (
-            id SERIAL PRIMARY KEY,
-            username VARCHAR(50) UNIQUE NOT NULL,
-            email VARCHAR(255) UNIQUE NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-        );
-    `);
+  const result = await client.query(
+    `
+        INSERT INTO users (username,email,password)
+        VALUES ($1,$2,$3)
+    `,
+    [username, email, password]
+  );
   console.log(result);
 }
 
-createUsersTable();
+createUsersTable("; DELETE * FROM users", "123", "hk1@gmail.com");
